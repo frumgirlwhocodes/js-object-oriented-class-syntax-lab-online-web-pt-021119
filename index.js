5caa846823006da55540c3a1498b2c3621a562c4
lieby weintraub <liebahinda@gmail.com>
2019-09-09 19:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ this.homestate= homeState
 this.training= training
 }
 
-veto(){  
-  return 'No, I must disagree';
-  
+
+
+	veto(){
+		return 'No, I must disagree';
+	}
+
+	approve(){
+		return 'You can do that!';
+	}
+
+	doCharity(){
+		return 'I like to help people.'
+	}
+
+	releasePressStatement(){
+		return 'You will see great things from Scuber.'
+	}
+
+	sayHi(){
+		return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+	}
+
 }
 
-approve(){
-  return 'You can do that!';
+class Ceo extends BoardMember {
+	hireEmployee(){
+		return 'Welcome aboard!'
+	}
 }
-
-doCharity() {
-  return 'I like to help people.'
-}
-
-releasePressStatement() {
-    return 'You will see great things from Scuber.'
-  }
-
-  sayHi() {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
-} 
-
-class Ceo extends BoardMember { 
-  hireEmployee(){  
-    return 'Welcome aboard!'  
-    } 
-} 
