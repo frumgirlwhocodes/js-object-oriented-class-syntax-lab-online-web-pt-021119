671236078fea05f3501b127e0dd4021af40b3fc9
lieby weintraub <liebahinda@gmail.com>
2019-09-09 19:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-class BoardMember{ 
-constructor(name, homeState, training){
-this.name= name
-this.homestate= homeState
-this.training= training
-}
+class BoardMember{
+	constructor (name, homeState, training){
+		this.name = name
+		this.homeState = homeState
+		this.training = training
+	}
 
 
 
